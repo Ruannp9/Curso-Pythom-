9c07f5158c5a572d03519c63487e1bd160081991
diimartell <migueluzumaki07@gmail.com>
2024-09-25 17:53:30 -0300

    Exercio-do-cartão-python
    Atividade de python 24/09/2024

---
+++ aula_2.continuaçao.py
@@ -0,0 +1,12 @@
+#entrada de dados
+
+Nome_hotel = input ("insira o nome do hotel")
+Nome_cidade = input ("insira o nome da cidade")
+Quantidade_estrelas = input ("insira a quantidade de estrelas de 1 a 5")
+
+print("*"*20,"/n" "*"*7, "Ibis" "/n" "*"*7,"*"*4, (f"{Quantidade_estrelas}") "*"*4,  )
+
+
+
+
+
---
+++ salario.py
@@ -0,0 +1,8 @@
+salarioBase = float(input("Salario base:"))
+gratificacao = float (input("Gratificação:"))
+
+#processamento
+salarioBruto = salarioBase + gratificacao
+
+#saida
+print("O valor do SalarioBruto é R$", salarioBruto)
