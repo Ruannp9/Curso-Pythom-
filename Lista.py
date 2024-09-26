b180cbb39f7a9deb14a32a919794a1fabbd14da8
diimartell <migueluzumaki07@gmail.com>
2024-09-25 21:16:34 -0300

    Listas

---
+++ Lista.py
@@ -0,0 +1,18 @@
+
+lista = [1,"2", 3] #adicionando itens a uma lista
+# print(type(lista)) #verificando o tipo da lista
+
+#declaração explícita de lista
+lista2 = list ((1,"2",3))
+# print (lista2)
+
+#Declarcão implicita
+lista3 = ["C", 4.65, True, "True", "vamos aprender", ["outra", "lista", "interna"], lista2]
+# print(lista3)
+
+lista4 =["primeiro", "segundo", "terceiro"]
+# print(lista4)
+
+#acessando uum elemento da lista
+print(lista3)
+print(lista3[-1][2]) #acessando a lista dentro da lista
