d99eddedd0dea7c5f05b4b06a0ca3c915f4c3b2d
diimartell <ruanvitoraquinooliveira@gmail.com>
2024-09-27 21:08:14 -0300

    Atividade de string
    Atividade de string 27/09/2024

---
+++ Atividade_lista/Atividadestring.py
@@ -0,0 +1,16 @@
+frase = "Estruturas de Dados em Python"
+
+maiusculo = frase.upper()
+print(maiusculo)
+
+indice = frase.index("Dados")
+print(indice)
+
+frase = "Estruturas de Dados em Python"
+nova_frase = frase.replace("Python", "Java")
+print(nova_frase)  # Saída: Estruturas de Dados em Java
+
+frase = "Estruturas de Dados em Python"
+Dividir = frase.split()
+
+print(Dividir)
