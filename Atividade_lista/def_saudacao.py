f2345f33c3701b76fae2596591b934ffdf01ddce
diimartell <ruanvitoraquinooliveira@gmail.com>
2024-10-02 21:05:34 -0300

    def
    def

---
+++ Atividade_lista/def_saudacao.py
@@ -0,0 +1,21 @@
+# def saudacao():
+# #     print("Olá, bem-vindo (a) à aula de funções em Python!")
+    
+# #para "chamar" a função e executar seu código:
+
+# def saudacao():
+
+# #Exemplo
+
+# def saudacao(nome): #'nome' é um parâmetro
+#     print(f"Olá, {nome}!")
+    
+# saudacao("carlos") #carlos é um argumento
+# saudacao("Ruan")#colocar outro nome sem apagar o anterior (só reptir o cod.)
+# #saída: Olá, Carlos!
+
+# #Exemplo
+# def somar (a, b, c, d): #mais de um parâmetro
+#     return a + b - c * d
+# resultado = somar (10,20, 5, 2)
+# print(resultado) #saída: 30
